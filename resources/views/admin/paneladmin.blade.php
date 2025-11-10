ea2458df6687d2ccac2a407c5aa80ba92b886724
Ian Felix <ianernesto031@gmail.com>
2025-11-10 15:13:52 -0700

    Cambios de panel de Admin y panel de empleados

--- resources/views/admin/paneladmin.blade.php
+++ resources/views/admin/paneladmin.blade.php
@@ -560,7 +560,8 @@ document.addEventListener('DOMContentLoaded', () => {
     </div>
   </div>
 
-   <!-- Scripts -->
+   <!--ssss Scripts -->
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.10/index.global.min.js"></script>
 
